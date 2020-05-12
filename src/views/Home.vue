27136372601db72d3389c46aff250b2b7fd2af9e
HweiH <hwhPiano@gmail.com>
2020-05-12 15:40:56 +0800

    modified Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <a href="https://github.com/HweiH/vue-bpmn" target="_blank" rel="noopener"><img alt="Vue logo" src="../assets/logo.png"></a>
     <MsgContent msg="Welcome to Your Vue.js App powered by bpmn.js integrated with Activiti"/>
   </div>
 </template>
